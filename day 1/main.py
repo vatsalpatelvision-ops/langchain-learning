a84f5c18d35644f20703304c94f1b88536614bc8
vatsalpatelvision-ops <vatsalpatel.vision@gmail.com>
2026-04-23 17:23:02 +0530

    using system message

--- day 1/main.py
+++ day 1/main.py
@@ -1,12 +1,35 @@
 from langchain_groq import ChatGroq
 from dotenv import load_dotenv
+from langchain_core.messages import SystemMessage,HumanMessage
 
 load_dotenv()
 
 llm = ChatGroq(
     model="llama-3.1-8b-instant"
 )
+# temperature = 0.2
+# max_tokens = 2
 
-response = llm.invoke("Explain 5 benifits of python in 1 line")
 
-print(response.content)
+while True:
+    user_input = input("Enter your question : ")
+
+    if user_input.lower() == "exit":
+        break
+    
+    # !normal plain text 
+    # response = llm.invoke(user_input)
+
+    # ! Text with roles
+    response = llm.invoke([
+        SystemMessage(content="You are a Strict coding teacher ! only answer quesetions related to coding"),
+        HumanMessage(content=user_input)
+    ])
+
+    print(f"AI : {response.content}")
+
+# print(response.content)
+# print("-"*30)
+# print(response.response_metadata)
+# print("-"*30)
+# print(response.response_metadata)
