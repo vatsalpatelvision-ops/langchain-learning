c9c4777ef516da4b09f4a996b6db01ace57dd7d5
vatsalpatelvision-ops <vatsalpatel.vision@gmail.com>
2026-04-24 10:16:11 +0530

    chaining multiple things

--- day 2/main2.py
+++ day 2/main2.py
@@ -8,12 +8,27 @@ model = ChatGroq(
     model="llama-3.1-8b-instant"
 )
 
-prompt = ChatPromptTemplate.from_template("Explain {topic} in simple words (max words 100)")
+# prompt = ChatPromptTemplate.from_template("Explain {topic} in simple words (max words 100)")
+# prompt = ChatPromptTemplate.from_template("Explain {topic} to {audience} in simple words")
+# prompt = ChatPromptTemplate.from_messages([
+#     ("system", "You are a strict teacher dont answer other then topic and give in strict and saccasting tone and in 100 words only"),
+#     ("human", "Explain {topic} to {audience}")
+# ])
 
-chain = prompt | model
+#! Chaining multiple steps like summarize then expalin in simple words
 
-user_input = input("For which topic you need simple explaination in 100 words : ")
+prompt = ChatPromptTemplate.from_template("Summarize this text : {text}")
 
-result = chain.invoke({"topic":user_input})
+prompt2 = ChatPromptTemplate.from_template("Explain this in simple words : {summary} in 100 words")
+
+
+chain = prompt | model | prompt2 | model
+
+user_input = input("Enter text for simple explaintion  : ")
+
+
+result = chain.invoke({
+    "text":user_input
+    })
 
 print(result.content)
